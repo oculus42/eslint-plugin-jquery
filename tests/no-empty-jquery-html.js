6b609cafc0356ad297f8e82b0ac05729bc910338
Samuel Rouse <samuel.rouse@gmail.com>
2016-10-12 10:43:52 -0400

    Update Rule Name
    Renamed for clarity.
    Update index.js for correctness.

--- tests/no-empty-jquery-html.js
+++ tests/no-empty-jquery-html.js
@@ -1,12 +1,12 @@
 'use strict';
 
-const rule = require('../rules/empty-html')
+const rule = require('../rules/no-empty-jquery-html')
 const RuleTester = require('eslint').RuleTester
 
 const error = 'Use $.empty() rather than $.html(\'\') or omit if you are later replacing the content.'
 
 const ruleTester = new RuleTester()
-ruleTester.run('empty-html', rule, {
+ruleTester.run('no-empty-jquery-html', rule, {
   valid: [
     '$("div").append("foo").html()',
     '$("div").html("<button>")',
